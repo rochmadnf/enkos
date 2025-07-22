8bc8ceb1e61e02578e6a16ae2295805f2503adf5
rochmadnf <rochmadnf@gmail.com>
2025-07-22 22:08:39 +0800

    login and logout method

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'username' => ['required', 'string', 'min:5'],
+            'password' => ['required', 'string', 'min:8'],
+        ];
+    }
+
+    public function authenticate()
+    {
+        if (! Auth::attempt($this->only('username', 'password'), $this->boolean('remember'))) {
+
+            throw ValidationException::withMessages([
+                'not_found' => trans('auth.failed'),
+            ]);
+        }
+    }
+}
