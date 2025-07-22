4e06ebf6e72940770d68097501f1cf0fd44ce313
rochmadnf <rochmadnf@gmail.com>
2025-07-22 22:06:01 +0800

    add new utils
    getInitialName

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function getInitialName(name: string) {
+    return name.match(/(^\S\S?|\b\S)?/g)?.join('').match(/(^\S|\S$)?/g)?.join('').toUpperCase();
+}
