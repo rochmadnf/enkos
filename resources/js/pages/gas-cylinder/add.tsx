fa102c3cfe62643741495172920870c158ffec2f
rochmadnf <rochmadnf@gmail.com>
2025-07-23 19:03:17 +0800

    refactor index and add gas cylinder --2

--- resources/js/pages/gas-cylinder/add.tsx
+++ resources/js/pages/gas-cylinder/add.tsx
@@ -1,32 +1,26 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+import { MainContent } from '@/components/main-content';
+import { CardContent } from '@/components/ui/card';
 import AppLayout from '@/layouts/app-layout';
-import { Head, Link } from '@inertiajs/react';
-import { PlusIcon } from 'lucide-react';
+import { Head } from '@inertiajs/react';
+import { ArrowLeft } from 'lucide-react';
 import { ReactNode } from 'react';
 
 export default function GasCylinderAdd() {
     return (
         <>
             <Head title="Tabung Gas">
-                <meta name="description" content="Daftar Tabung Gas" />
+                <meta name="description" content="Tambah Tabung" />
             </Head>
-            <Card className="min-h-full flex-1 rounded-2xl border-app-primary-300 px-6 shadow-app-primary-600/30">
-                <div className="flex flex-row items-center justify-between" data-slot="card-header">
-                    <div className="space-y-1">
-                        <h1 className="text-3xl font-semibold tracking-wide">Daftar Tabung</h1>
-                        <p className="font-light tracking-wider text-slate-900">Menampilkan tabung yang sudah tersimpan di sistem.</p>
-                    </div>
-                    <Button size={'icon'} asChild className="group/button size-11 rounded-full">
-                        <Link href={'#'}>
-                            <PlusIcon className="size-6" />
-                        </Link>
-                    </Button>
-                </div>
-                <CardContent>
-                    <p>content</p>
-                </CardContent>
-            </Card>
+            <MainContent
+                contentName="Tambah Tabung"
+                contentDesc="Form penambahan tabung gas."
+                buttonVariant={'netral'}
+                buttonIcon={<ArrowLeft />}
+                buttonLabel="Kembali"
+                url={route('gas_cylinder.index')}
+            >
+                <CardContent>ABCD</CardContent>
+            </MainContent>
         </>
     );
 }
--- resources/js/pages/gas-cylinder/index.tsx
+++ resources/js/pages/gas-cylinder/index.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+import { MainContent } from '@/components/main-content';
+import { CardContent } from '@/components/ui/card';
 import AppLayout from '@/layouts/app-layout';
-import { Head, Link } from '@inertiajs/react';
-import { PlusIcon } from 'lucide-react';
+import { Head } from '@inertiajs/react';
+import { Plus } from 'lucide-react';
 import { ReactNode } from 'react';
 
 export default function GasCylinderIndex() {
@@ -11,22 +11,15 @@ export default function GasCylinderIndex() {
             <Head title="Tabung Gas">
                 <meta name="description" content="Daftar Tabung Gas" />
             </Head>
-            <Card className="min-h-full flex-1 rounded-2xl border-app-primary-300 px-6 shadow-app-primary-600/30">
-                <div className="flex flex-row items-center justify-between" data-slot="card-header">
-                    <div className="space-y-1">
-                        <h1 className="text-3xl font-semibold tracking-wide">Daftar Tabung</h1>
-                        <p className="font-light tracking-wider text-slate-900">Menampilkan tabung yang sudah tersimpan di sistem.</p>
-                    </div>
-                    <Button size={'icon'} asChild className="group/button size-11 rounded-full">
-                        <Link href={route('gas_cylinder.create')}>
-                            <PlusIcon className="size-6" />
-                        </Link>
-                    </Button>
-                </div>
-                <CardContent>
-                    <p>content</p>
-                </CardContent>
-            </Card>
+            <MainContent
+                contentName="Daftar Tabung"
+                contentDesc="Menampilkan tabung yang sudah tersimpan di sistem."
+                buttonIcon={<Plus />}
+                buttonLabel="Tambah Data"
+                url={route('gas_cylinder.create')}
+            >
+                <CardContent>ABCDe</CardContent>
+            </MainContent>
         </>
     );
 }
