9aa3b3561a615073bb653cf71e00575ebec6d251
rochmadnf <rochmadnf@gmail.com>
2025-08-06 23:27:40 +0800

    feat(gl): refactor controller to clean code

--- app/Http/Controllers/GasLocationController.php
+++ app/Http/Controllers/GasLocationController.php
@@ -3,28 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Enums\GasLocation\TypeEnum;
-use App\Http\Requests\GasLocation\UpdateRequest;
+use App\Http\Requests\GasLocation\{StoreRequest, UpdateRequest};
 use App\Http\Resources\GasLocationResource;
+use App\Repositories\Contracts\GasLocationRepositoryInterface;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Inertia\Response as InertiaResponse;
 
 class GasLocationController extends Controller
 {
-    public function __construct(protected \App\Repositories\Contracts\GasLocationRepositoryInterface $gasLocationRepo)
-    {
-        //
-    }
+    public function __construct(protected GasLocationRepositoryInterface $gasLocationRepo) {}
 
     public function index(): InertiaResponse
     {
         return inertia('gas-location/index', [
-            'page' => [
-                'uuid' => 'mni_003',
-                'name' => ($pageName = 'Lokasi Tabung'),
-                'description' => 'Menampilkan seluruh daftar lokasi tabung gas.',
-                'breadcrumbs' => [['id' => 'glbrc_001', 'href' => '#', 'label' => $pageName]],
-            ],
+            'page' => $this->pageMeta('Lokasi Tabung', 'Menampilkan seluruh daftar lokasi tabung gas.'),
             'resources' => $this->gasLocationRepo->paginate(),
         ]);
     }
@@ -32,20 +24,14 @@ class GasLocationController extends Controller
     public function create(): InertiaResponse
     {
         return inertia('gas-location/add', [
-            'page' => [
-                'uuid' => 'mni_003',
-                'name' => ($pageName = 'Tambah Lokasi'),
-                'description' => 'Form untuk menambah lokasi tabung gas.',
-                'breadcrumbs' => [
-                    ['id' => 'glbrc_001', 'href' => route('gas_location.index'), 'label' => 'Lokasi Tabung'],
-                    ['id' => 'glbrc_002', 'href' => '#', 'label' => $pageName],
-                ],
-            ],
+            'page' => $this->pageMeta('Tambah Lokasi', 'Form untuk menambah lokasi tabung gas.', [
+                ['id' => 'glbrc_001', 'href' => route('gas_location.index'), 'label' => 'Lokasi Tabung'],
+            ]),
             'type' => TypeEnum::toArray(),
         ]);
     }
 
-    public function store(\App\Http\Requests\GasLocation\StoreRequest $request)
+    public function store(StoreRequest $request): RedirectResponse
     {
         $this->gasLocationRepo->create($request->validated());
         return to_route('gas_location.index');
@@ -54,18 +40,13 @@ class GasLocationController extends Controller
     public function edit(string $id): InertiaResponse
     {
         $gasLocation = $this->gasLocationRepo->find($id);
+
         return inertia('gas-location/edit', [
-            'page' => [
-                'uuid' => 'mni_003',
-                'name' => ($pageName = 'Ubah Data Lokasi'),
-                'description' => 'Form untuk ubah data lokasi tabung gas.',
-                'breadcrumbs' => [
-                    ['id' => 'glbrc_001', 'href' => route('gas_location.index'), 'label' => 'Lokasi Tabung'],
-                    ['id' => 'glbrc_002', 'href' => '#', 'label' => $pageName],
-                ],
-            ],
+            'page' => $this->pageMeta('Ubah Data Lokasi', 'Form untuk ubah data lokasi tabung gas.', [
+                ['id' => 'glbrc_001', 'href' => route('gas_location.index'), 'label' => 'Lokasi Tabung'],
+            ]),
             'type' => TypeEnum::toArray(),
-            'location' => GasLocationResource::make($gasLocation),
+            'location' => new GasLocationResource($gasLocation),
         ]);
     }
 
@@ -79,5 +60,18 @@ class GasLocationController extends Controller
     {
         $this->gasLocationRepo->delete($id);
         return to_route('gas_location.index', request()->query());
+    }
+
+    /**
+     * Generate page meta info.
+     */
+    private function pageMeta(string $name, string $description, array $breadcrumbs = []): array
+    {
+        return [
+            'uuid' => 'mni_003',
+            'name' => $name,
+            'description' => $description,
+            'breadcrumbs' => array_merge($breadcrumbs, [['id' => 'glbrc_last', 'href' => '#', 'label' => $name]]),
+        ];
     }
 }
