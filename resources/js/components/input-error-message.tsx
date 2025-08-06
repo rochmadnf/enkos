9cf40dd7ff82ea59fbc4a3a53ef4407b2c7e0263
rochmadnf <rochmadnf@gmail.com>
2025-08-06 23:12:32 +0800

    feat(component): Ensure error messages don't break form alignment

--- resources/js/components/input-error-message.tsx
+++ resources/js/components/input-error-message.tsx
@@ -1,9 +1,12 @@
 import { cn } from '@/lib/utils';
 import { HTMLAttributes } from 'react';
 
-export function InputErrorMessage({ className, message, ...props }: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
+export function InputErrorMessage({ className, message = 'error text', ...props }: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
     return (
-        <p {...props} className={cn('mt-1 text-sm text-destructive first-letter:uppercase', className)}>
+        <p
+            {...props}
+            className={cn('invisible mt-1 text-sm text-destructive first-letter:uppercase', message !== 'error text' ? 'visible' : null, className)}
+        >
             {message}
         </p>
     );
