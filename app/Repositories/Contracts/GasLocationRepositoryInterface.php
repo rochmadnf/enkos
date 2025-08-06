29f4f71affeffb3b7abeb45b7be15fcfa55261f1
rochmadnf <rochmadnf@gmail.com>
2025-08-06 23:05:06 +0800

    feat(pattern): GasLocationRepository
    - find
    - paginate (search)
    - update
    - create
    - delete

---
+++ app/Repositories/Contracts/GasLocationRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface GasLocationRepositoryInterface
+{
+    public function paginate();
+
+    public function find(string $id);
+
+    public function create(array $validated);
+
+    public function delete(string $id);
+
+    public function update(string $id, array $validated);
+}
---
+++ app/Repositories/GasLocationRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Resources\GasLocationResource;
+use App\Models\GasLocation;
+use App\Repositories\Contracts\GasLocationRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
+
+class GasLocationRepository implements GasLocationRepositoryInterface
+{
+    public function find(string $id)
+    {
+        $gasLocation = GasLocation::find($id);
+
+        if (!$gasLocation) {
+            throw ValidationException::withMessages([
+                'message' => 'Data Lokasi tidak ditemukan.',
+            ]);
+        }
+
+        return $gasLocation;
+    }
+
+    public function paginate()
+    {
+        return GasLocationResource::collection(
+            GasLocation::when(request()->has('keyword') && request()->get('keyword'), function (Builder $query) {
+                $query->where('name', 'LIKE', '%' . request()->get('keyword') . '%');
+            })->paginate(perPage: request()->has('per_page') ? request()->get('per_page') : 8),
+        );
+    }
+
+    public function create(array $validated)
+    {
+        try {
+            return DB::transaction(fn() => GasLocation::create($validated));
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function delete(string $id)
+    {
+        return $this->find($id)->delete();
+    }
+
+    public function update(string $id, array $validated)
+    {
+        return $this->find($id)->update($validated);
+    }
+}
